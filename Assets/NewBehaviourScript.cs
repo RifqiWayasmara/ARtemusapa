7848a261ef181543f3382a891cf712cc7dcb823c
RifqiWayasmara <99477899+RifqiWayasmara@users.noreply.github.com>
2022-02-11 17:52:03 +0700

    Add files via upload

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public void GoTomenu()
+    {
+        Application.LoadLevel("menu");
+    }
+
+    public void GoToar()
+    {
+        Application.LoadLevel("ar");
+    }
+
+    public void GoToabout()
+    {
+        Application.LoadLevel("about");
+    }
+
+    public void GoTohelp()
+    {
+        Application.LoadLevel("help");
+    }
+
+    public void GoToexit()
+    {
+        Application.Quit();
+    }
+}
